3292c27d78cf2476debfacc1db39b48a1f6f7d48
Robert <dokhoyanrobert1@gmail.com>
2026-04-18 23:49:37 +0400

    first commit

--- app/src/main/java/com/example/rodi/MainActivity.java
+++ app/src/main/java/com/example/rodi/MainActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -23,41 +20,36 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
         bottomNavigationView = findViewById(R.id.BottomNavigationMenu);
         fab = findViewById(R.id.fab);
 
         if (savedInstanceState == null) {
             loadFragment(new HomeFragment());
         }
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            int id = item.getItemId();
 
-            if (id == R.id.home) {
-                loadFragment(new HomeFragment());
-                return true;
-            } else if (id == R.id.profile) {
-                loadFragment(new ProfileFragment());
-                return true;
-            } else if (id == R.id.record) {
-                loadFragment(new OthermenuFragment());
-                return true;
-            } else if (id == R.id.settings) {
-                loadFragment(new SettingsFragment());
-                return true;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.home) {
+                selectedFragment = new HomeFragment();
+            } else if (item.getItemId() == R.id.profile) {
+                selectedFragment = new ProfileFragment();
+            } else if (item.getItemId() == R.id.record) {
+                selectedFragment = new RecordFragment();
+            } else if (item.getItemId() == R.id.settings) {
+                selectedFragment = new SettingsFragment();
             }
 
-            return false;
+            if (selectedFragment != null) {
+                loadFragment(selectedFragment);
+            }
+
+            return true;
         });
-        fab.setOnClickListener(v -> {
-            loadFragment(new RecordFragment());
-        });
+
+        fab.setOnClickListener(v -> loadFragment(new OthermenuFragment()));
     }
+
     private void loadFragment(Fragment fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
